4d79a1389d56c289fa3012db8ded36432d2da16f
grandcatswinegrapes <grandcatswinegrapes@gmail.com>
2020-08-25 23:50:38 +0300

    Adding IMAP.ts

--- src/IMAP.ts
+++ src/IMAP.ts
@@ -43,4 +43,54 @@ export class Worker {
         await client.connect();
         return client;
     }
+
+    public async listMailboxes(): Promise<IMailbox[]> {
+        const client: any = await this.connectToServer();
+        const mailboxes: any = await client.listMailboxes();
+        await client.close;
+        const finalMailboxes: IMailbox[] = [];
+        const iterateChildren: Function = (inArray: any[]): void => {
+            inArray.forEach((inValue: any) => {
+                finalMailboxes.push({
+                    name: inValue.name, 
+                    path: inValue.path
+                });
+                iterateChildren(inValue.children);
+            })
+        }
+        iterateChildren(mailboxes.children)
+        return finalMailboxes;
+    }
+
+    public async listMessages(inCallOptions: ICallOptions): Promise<IMessage[]> {
+        const client: any = await this.connectToServer();
+        const mailbox: any = await client.selectMailbox(inCallOptions.mailbox);
+        if (mailbox.exists === 0) {
+            await client.close();
+            return [];
+        }
+        const messages: any[] = await client.listMessages(inCallOptions.mailbox, "1:*", ["uid", "envelope"])
+        await client.close();
+        const finalMessages: IMessage[] = [];
+        messages.forEach((inValue: any) => {
+            finalMessages.push({
+                id: inValue.uid, 
+                date: inValue.envelope.date,
+                from: inValue.envelope.from[0].adress,
+                subject: inValue.envelope.subject
+            })
+        })
+        return finalMessages;
+    }
+
+    public async getMessageBody(inCallOptions: ICallOptions): Promise<string> {
+        const client: any = await this.connectToServer();
+        const messages: any[] = await client.listMessages(inCallOptions.mailbox, inCallOptions.id, ["body[]"], {byUid: true})
+        const parsed: ParsedMail = await simpleParser(messages[0]["body[]"])
+        await client.close();
+        if (parsed.text)
+            return parsed.text;
+        else
+            return `Error: message body is undefined` //page: 218
+    }
 }
